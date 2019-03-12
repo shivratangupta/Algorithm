902a6db5c3bc6f328b977e15377e2686baa541c9
shivratangupta <shivratangupta18@gmail.com>
2019-03-12 20:08:24 +0530

    added better solution of PermutationSequenc

--- PermutationSequence.cpp
+++ PermutationSequence.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string.h>
+#include<vector>
 #include<algorithm>
 using namespace std;
 
@@ -8,10 +9,26 @@ string getPermutation(int n, int k){
 	for(int i=1; i<=n; i++)
 		s += i + '0';
 
-	for(int i=1; i<k; i++)
-		next_permutation(s.begin(), s.end());
+	vector<int> factorial(n + 1, 1);
+	int fact;
+	fact = 1;
+	for(int i=1; i<=n; i++){
+		fact *= i;
+		factorial[i] = fact;
+	}
 
-	return s;
+	k--;
+
+	string result = "";
+	for(int i=1; i<=n; i++){
+		int index;
+		index = k / factorial[n - i];
+		result += s[index];
+		s.erase(s.begin() + index);
+		k -= index * factorial[n - i];
+	}
+
+	return result;
 }
 
 int main(){
